fc7faaa2c2f8a9625e87f678e8880072833d2cb8
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-05 16:13:55 +0300

    favorite_product

--- app/models/User.rb
+++ app/models/User.rb
@@ -1,5 +1,9 @@
 class User < ActiveRecord::Base
     has_many :reviews
     has_many :products, through: :reviews
+  
+    def favorite_product
+      products.joins(:reviews).where(reviews: {user_id: id}).order('reviews.star_rating DESC').first
+    end
+  
 end
-    
