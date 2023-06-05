cae81f40725e773cf9851b50892fc9ca2582dbec
nicolesanaipei <nicole.sanaipei@student.moringaschools.com>
2023-06-05 16:02:53 +0300

    leave_review

--- app/models/Product.rb
+++ app/models/Product.rb
@@ -1,5 +1,9 @@
 class Product < ActiveRecord::Base
   has_many :reviews
   has_many :users, through: :reviews
+
+  def leave_review(user, star_rating, comment)
+      reviews.create(user: user, star_rating: star_rating, comment: comment)
+  end
+
 end
-  
